0fbf401c98d1c95c7ed6c2118be08afbbb68359c
Young <afe.young@gmail.com>
2020-11-26 12:40:50 +0000

    update trainer and README.md

--- qlib/workflow/cli.py
+++ qlib/workflow/cli.py
@@ -8,9 +8,7 @@ import qlib
 import fire
 import pandas as pd
 import ruamel.yaml as yaml
-from qlib.utils import init_instance_by_config, flatten_dict
-from qlib.workflow import R
-from qlib.workflow.record_temp import SignalRecord
+from ..model.trainer import task_train
 
 
 def get_path_list(path):
@@ -54,29 +52,7 @@ def workflow(config_path, experiment_name="workflow"):
     region = config.get("region")
     qlib.init(provider_uri=provider_uri, region=region)
 
-    # model initiaiton
-    model = init_instance_by_config(config.get("task")["model"])
-    dataset = init_instance_by_config(config.get("task")["dataset"])
-
-    # start exp
-    with R.start(experiment_name=experiment_name):
-        # train model
-        R.log_params(**flatten_dict(config.get("task")))
-        model.fit(dataset)
-        recorder = R.get_recorder()
-
-        # generate records: prediction, backtest, and analysis
-        for record in config.get("task")["record"]:
-            if record["class"] == SignalRecord.__name__:
-                srconf = {"model": model, "dataset": dataset, "recorder": recorder}
-                record["kwargs"].update(srconf)
-                sr = init_instance_by_config(record)
-                sr.generate()
-            else:
-                rconf = {"recorder": recorder}
-                record["kwargs"].update(rconf)
-                ar = init_instance_by_config(record)
-                ar.generate()
+    task_train(config, experiment_name=experiment_name)
 
 
 # function to run worklflow by config
