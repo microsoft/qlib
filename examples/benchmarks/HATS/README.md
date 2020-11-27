aa2b28386aca03b8f953bc5005ee8cdf0b1ae744
Hong Zhang <v-hozhan@microsoft.com>
2020-11-28 02:04:35 +0800

    revised HATS

---
+++ examples/benchmarks/HATS/README.md
@@ -0,0 +1,12 @@
+## Requirement
+
+* pandas==1.1.2
+* numpy==1.17.4
+* scikit_learn==0.23.2
+* torch==1.7.0
+
+## HATS
+
+* HATS is a a hierarchical attention network for stock prediction which uses attention layers to broadcast weight for stock market prediction. HATS selectively aggregates information on different relation types and adds the information to the representations of each company. HATS is used as a module with initialized node representations.Furthermore, HATS can  predict not only individual stock prices but also market index movements, which is similar to the graph classification task.
+* HATS uses pretrained model of GRU and LSTM. The code of GRU and LSTM used in Qlib is a pyTorch implemention of GRU and LSTM.
+* Paper address:HATS: A Hierarchical Graph Attention Network for Stock Movement Prediction https://arxiv.org/pdf/1908.07999.pdf
--- qlib/contrib/model/pytorch_hats.py
+++ qlib/contrib/model/pytorch_hats.py
@@ -1,5 +1,14 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 from __future__ import division
@@ -12,7 +21,6 @@ import copy
 from ...utils import create_save_path
 from ...log import get_module_logger
 
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -20,20 +28,16 @@ import torch.optim as optim
 from ...model.base import Model
 from ...data.dataset import DatasetH
 from ...data.dataset.handler import DataHandlerLP
-from ...contrib.model.pytorch_lstm import LSTMModel
-from ...contrib.model.pytorch_gru import GRUModel
 
 
-class GATs(Model):
-    """GATs Model
+class HATS(Model):
+    """HATS Model
 
     Parameters
     ----------
-    lr : float
-        learning rate
     d_feat : int
-        input dimensions for each time step
-    metric : str
+        input dimension for each time step
+    metric: str
         the evaluate metric used in early stop
     optimizer : str
         optimizer name
@@ -46,9 +50,9 @@ class GATs(Model):
         d_feat=6,
         hidden_size=64,
         num_layers=2,
-        dropout=0.0,
+        dropout=0.5,
         n_epochs=200,
-        lr=0.001,
+        lr=0.01,
         metric="",
         early_stop=20,
         loss="mse",
@@ -60,8 +64,8 @@ class GATs(Model):
         **kwargs
     ):
         # Set logger.
-        self.logger = get_module_logger("GATs")
-        self.logger.info("GATs pytorch version...")
+        self.logger = get_module_logger("HATS")
+        self.logger.info("HATS pytorch version...")
 
         # set hyper-parameters.
         self.d_feat = d_feat
@@ -81,7 +85,7 @@ class GATs(Model):
         self.seed = seed
 
         self.logger.info(
-            "GATs parameters setting:"
+            "HATS parameters setting:"
             "\nd_feat : {}"
             "\nhidden_size : {}"
             "\nnum_layers : {}"
@@ -115,7 +119,7 @@ class GATs(Model):
             )
         )
 
-        self.GAT_model = GATModel(
+        self.HATS_model = HATSModel(
             d_feat=self.d_feat,
             hidden_size=self.hidden_size,
             num_layers=self.num_layers,
@@ -123,15 +127,15 @@ class GATs(Model):
             base_model=self.base_model,
         )
         if optimizer.lower() == "adam":
-            self.train_optimizer = optim.Adam(self.GAT_model.parameters(), lr=self.lr)
+            self.train_optimizer = optim.Adam(self.HATS_model.parameters(), lr=self.lr)
         elif optimizer.lower() == "gd":
-            self.train_optimizer = optim.SGD(self.GAT_model.parameters(), lr=self.lr)
+            self.train_optimizer = optim.SGD(self.HATS_model.parameters(), lr=self.lr)
         else:
             raise NotImplementedError("optimizer {} is not supported!".format(optimizer))
 
         self._fitted = False
         if self.use_gpu:
-            self.GAT_model.cuda()
+            self.HATS_model.cuda()
             # set the visible GPU
             if self.visible_GPU:
                 os.environ["CUDA_VISIBLE_DEVICES"] = self.visible_GPU
@@ -149,21 +153,20 @@ class GATs(Model):
         raise ValueError("unknown loss `%s`" % self.loss)
 
     def metric_fn(self, pred, label):
-
         mask = torch.isfinite(label)
 
-        if self.metric == "" or self.metric == "loss":
+        if self.metric == "" or self.metric == "loss":  # use loss
             return -self.loss_fn(pred[mask], label[mask])
 
         raise ValueError("unknown metric `%s`" % self.metric)
 
     def get_daily_inter(self, df, shuffle=False):
-        # organize the train data into daily batches
+        # organize the train data into daily inter as daily batches
         daily_count = df.groupby(level=0).size().values
         daily_index = np.roll(np.cumsum(daily_count), 1)
         daily_index[0] = 0
         if shuffle:
-            # shuffle data
+            # shuffle the daily inter data
             daily_shuffle = list(zip(daily_index, daily_count))
             np.random.shuffle(daily_shuffle)
             daily_index, daily_count = zip(*daily_shuffle)
@@ -173,9 +176,10 @@ class GATs(Model):
 
         x_train_values = x_train.values
         y_train_values = np.squeeze(y_train.values)
-        self.GAT_model.train()
 
-        # organize the train data into daily batches
+        self.HATS_model.train()
+
+        # organize the train data into daily inter as daily batches
         daily_index, daily_count = self.get_daily_inter(x_train, shuffle=True)
 
         for idx, count in zip(daily_index, daily_count):
@@ -187,26 +191,26 @@ class GATs(Model):
                 feature = feature.cuda()
                 label = label.cuda()
 
-            pred = self.GAT_model(feature)
+            pred = self.HATS_model(feature)
             loss = self.loss_fn(pred, label)
 
             self.train_optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_value_(self.GAT_model.parameters(), 3.0)
+            torch.nn.utils.clip_grad_value_(self.HATS_model.parameters(), 3.0)
             self.train_optimizer.step()
 
     def test_epoch(self, data_x, data_y):
 
-        # prepare training data
+        # prepare testing data
         x_values = data_x.values
         y_values = np.squeeze(data_y.values)
 
-        self.GAT_model.eval()
+        self.HATS_model.eval()
 
         scores = []
         losses = []
 
-        # organize the test data into daily batches
+        # organize the test data into daily inter as daily batches
         daily_index, daily_count = self.get_daily_inter(data_x, shuffle=False)
 
         for idx, count in zip(daily_index, daily_count):
@@ -218,7 +222,7 @@ class GATs(Model):
                 feature = feature.cuda()
                 label = label.cuda()
 
-            pred = self.GAT_model(feature)
+            pred = self.HATS_model(feature)
             loss = self.loss_fn(pred, label)
             losses.append(loss.item())
 
@@ -236,9 +240,7 @@ class GATs(Model):
     ):
 
         df_train, df_valid, df_test = dataset.prepare(
-            ["train", "valid", "test"],
-            col_set=["feature", "label"],
-            data_key=DataHandlerLP.DK_L,
+            ["train", "valid", "test"], col_set=["feature", "label"], data_key=DataHandlerLP.DK_L
         )
 
         x_train, y_train = df_train["feature"], df_train["label"]
@@ -256,17 +258,19 @@ class GATs(Model):
         if self.with_pretrain:
             self.logger.info("Loading pretrained model...")
             if self.base_model == "LSTM":
+                from ...contrib.model.pytorch_lstm import LSTMModel
+
                 pretrained_model = LSTMModel()
                 pretrained_model.load_state_dict(torch.load("benchmarks/LSTM/model_lstm_csi300.pkl"))
-
             elif self.base_model == "GRU":
+                from ...contrib.model.pytorch_gru import GRUModel
+
                 pretrained_model = GRUModel()
                 pretrained_model.load_state_dict(torch.load("benchmarks/GRU/model_gru_csi300.pkl"))
-
-            model_dict = self.GAT_model.state_dict()
+            model_dict = self.HATS_model.state_dict()
             pretrained_dict = {k: v for k, v in pretrained_model.state_dict().items() if k in model_dict}
             model_dict.update(pretrained_dict)
-            self.GAT_model.load_state_dict(model_dict)
+            self.HATS_model.load_state_dict(model_dict)
             self.logger.info("Loading pretrained model Done...")
 
         # train
@@ -288,7 +292,7 @@ class GATs(Model):
                 best_score = val_score
                 stop_steps = 0
                 best_epoch = step
-                best_param = copy.deepcopy(self.GAT_model.state_dict())
+                best_param = copy.deepcopy(self.HATS_model.state_dict())
             else:
                 stop_steps += 1
                 if stop_steps >= self.early_stop:
@@ -296,7 +300,7 @@ class GATs(Model):
                     break
 
         self.logger.info("best score: %.6lf @ %d" % (best_score, best_epoch))
-        self.GAT_model.load_state_dict(best_param)
+        self.HATS_model.load_state_dict(best_param)
         torch.save(best_param, save_path)
 
         if self.use_gpu:
@@ -308,11 +312,12 @@ class GATs(Model):
 
         x_test = dataset.prepare("test", col_set="feature")
         index = x_test.index
-        self.GAT_model.eval()
+        self.HATS_model.eval()
         x_values = x_test.values
+        sample_num = x_values.shape[0]
         preds = []
 
-        # organize the data into daily batches
+        # organize the data into daily inter as daily batches
         daily_index, daily_count = self.get_daily_inter(x_test, shuffle=False)
 
         for idx, count in zip(daily_index, daily_count):
@@ -324,21 +329,21 @@ class GATs(Model):
 
             with torch.no_grad():
                 if self.use_gpu:
-                    pred = self.GAT_model(x_batch).detach().cpu().numpy()
+                    pred = self.HATS_model(x_batch).detach().cpu().numpy()
                 else:
-                    pred = self.GAT_model(x_batch).detach().numpy()
+                    pred = self.HATS_model(x_batch).detach().numpy()
 
             preds.append(pred)
 
         return pd.Series(np.concatenate(preds), index=index)
 
 
-class GATModel(nn.Module):
+class HATSModel(nn.Module):
     def __init__(self, d_feat=6, hidden_size=64, num_layers=2, dropout=0.0, base_model="GRU"):
         super().__init__()
 
         if base_model == "GRU":
-            self.rnn = nn.GRU(
+            self.model = nn.GRU(
                 input_size=d_feat,
                 hidden_size=hidden_size,
                 num_layers=num_layers,
@@ -346,7 +351,7 @@ class GATModel(nn.Module):
                 dropout=dropout,
             )
         elif base_model == "LSTM":
-            self.rnn = nn.LSTM(
+            self.model = nn.LSTM(
                 input_size=d_feat,
                 hidden_size=hidden_size,
                 num_layers=num_layers,
@@ -357,38 +362,130 @@ class GATModel(nn.Module):
             raise ValueError("unknown base model name `%s`" % base_model)
 
         self.hidden_size = hidden_size
-        self.d_feat = d_feat
-        self.transformation = nn.Linear(self.hidden_size, self.hidden_size)
-        self.a = nn.Parameter(torch.randn(self.hidden_size * 2, 1))
-        self.a.requires_grad = True
-        self.fc = nn.Linear(self.hidden_size, self.hidden_size)
+        self.bn1 = nn.BatchNorm1d(num_features=hidden_size, track_running_stats=False)
+        self.fc = nn.Linear(hidden_size, hidden_size)
+        self.bn2 = nn.BatchNorm1d(num_features=hidden_size, track_running_stats=False)
         self.fc_out = nn.Linear(hidden_size, 1)
         self.leaky_relu = nn.LeakyReLU()
         self.softmax = nn.Softmax(dim=1)
+        self.d_feat = d_feat
 
-    def cal_attention(self, x, y):
-        x = self.transformation(x)
-        y = self.transformation(y)
-
-        sample_num = x.shape[0]
-        dim = x.shape[1]
-        e_x = x.expand(sample_num, sample_num, dim)
-        e_y = torch.transpose(e_x, 0, 1)
-        attention_in = torch.cat((e_x, e_y), 2).view(-1, dim * 2)
-        self.a_t = torch.t(self.a)
-        attention_out = self.a_t.mm(torch.t(attention_in)).view(sample_num, sample_num)
-        attention_out = self.leaky_relu(attention_out)
-        att_weight = self.softmax(attention_out)
-        return att_weight
+        num_head_att = [1] * num_layers
+        hidden_dim = [hidden_size] * num_layers
+        dims = [d_feat] + [d * nh for (d, nh) in zip(hidden_dim, num_head_att[:-1])] + [num_head_att[-1]]
+        in_dims = dims[:-1]
+        out_dims = [d // nh for (d, nh) in zip(dims[1:], num_head_att)]
+        self.attn = nn.ModuleList(
+            [GraphAttention(i, o, nh, dropout) for (i, o, nh) in zip(in_dims, out_dims, num_head_att)]
+        )
+        self.bns = nn.ModuleList([nn.BatchNorm1d(dim) for dim in dims[1:-1]])
+        self.dropout = nn.Dropout(dropout)
+        self.elu = nn.ELU()
 
     def forward(self, x):
-        # x: [N, F*T]
         x = x.reshape(len(x), self.d_feat, -1)  # [N, F, T]
         x = x.permute(0, 2, 1)  # [N, T, F]
-        out, _ = self.rnn(x)
+        out, _ = self.model(x)
         hidden = out[:, -1, :]
-        att_weight = self.cal_attention(hidden, hidden)
-        hidden = att_weight.mm(hidden) + hidden
-        hidden = self.fc(hidden)
-        hidden = self.leaky_relu(hidden)
-        return self.fc_out(hidden).squeeze()
+        hidden = self.bn1(hidden)
+        attention = GraphAttention.cal_attention(hidden, hidden)
+        output = attention.mm(hidden)
+        output = self.fc(output)
+        output = self.bn2(output)
+        output = self.leaky_relu(output)
+        return self.fc_out(output).squeeze()
+
+
+class GraphAttention(nn.Module):
+    def __init__(self, input_dim, output_dim, num_heads, dropout=0.5):
+
+        super().__init__()
+
+        """
+        Parameters
+        ----------
+        input_dim : int
+            Dimension of input node features.
+        output_dim : int
+            Dimension of output node features.
+        num_heads : list of ints
+            Number of attention heads in each hidden layer and output layer. Must be non empty. Note that len(num_heads) = len(hidden_dims)+1.
+        dropout : float
+            Dropout rate. Default: 0.5.
+        """
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.num_heads = num_heads
+
+        self.fcs = nn.ModuleList([nn.Linear(input_dim, output_dim) for _ in range(num_heads)])
+        self.a = nn.ModuleList([nn.Linear(2 * output_dim, 1) for _ in range(num_heads)])
+
+        self.dropout = nn.Dropout(dropout)
+        self.softmax = nn.Softmax(dim=0)
+        self.leakyrelu = nn.LeakyReLU()
+
+    def forward(self, features, nodes, mappings, rows):
+
+        """
+        Parameters
+        ----------
+        features : torch.Tensor
+            An (n' x input_dim) tensor of input node features.
+        nodes : list of numpy array
+            nodes[i] is an array of the nodes in the ith layer of the
+            computation graph.
+        mappings : list of dictionary
+            mappings[i] is a dictionary mappings node v (labelled 0 to |V|-1)
+            in nodes[i] to its position in nodes[i]. For example,
+            if nodes[i] = [2,5], then mappings[i][2] = 0 and
+            mappings[i][5] = 1.
+        rows : numpy array
+            rows[i] is an array of neighbors of node i.
+        Returns
+        -------
+        out : torch.Tensor
+            An (len(node_layers[-1]) x output_dim) tensor of output node features.
+        """
+
+        nprime = features.shape[0]
+        rows = [np.array([mappings[v] for v in row], dtype=np.int64) for row in rows]
+        sum_degs = np.hstack(([0], np.cumsum([len(row) for row in rows])))
+        mapped_nodes = [mappings[v] for v in nodes]
+        indices = torch.LongTensor([[v, c] for (v, row) in zip(mapped_nodes, rows) for c in row]).t()
+
+        out = []
+        for k in range(self.num_heads):
+            h = self.fcs[k](features)
+
+            nbr_h = torch.cat(tuple([h[row] for row in rows]), dim=0)
+            self_h = torch.cat(
+                tuple([h[mappings[nodes[i]]].repeat(len(row), 1) for (i, row) in enumerate(rows)]), dim=0
+            )
+            cat_h = torch.cat((self_h, nbr_h), dim=1)
+
+            e = self.leakyrelu(self.a[k](cat_h))
+
+            alpha = [self.softmax(e[lo:hi]) for (lo, hi) in zip(sum_degs, sum_degs[1:])]
+            alpha = torch.cat(tuple(alpha), dim=0)
+            alpha = alpha.squeeze(1)
+            alpha = self.dropout(alpha)
+
+            adj = torch.sparse.FloatTensor(indices, alpha, torch.Size([nprime, nprime]))
+            out.append(torch.sparse.mm(adj, h)[mapped_nodes])
+
+        return out
+
+    @staticmethod
+    def cal_attention(x, y):
+        att_x = torch.mean(x, dim=1).reshape(-1, 1)
+        att_y = torch.mean(y, dim=1).reshape(-1, 1)
+        att = att_x.mm(torch.t(att_y))
+        return (
+            torch.mean(
+                x.reshape(x.shape[0], 1, x.shape[1]).repeat(1, y.shape[0], 1)
+                * y.reshape(1, y.shape[0], y.shape[1]).repeat(x.shape[0], 1, 1),
+                dim=2,
+            )
+            - att
+        )
