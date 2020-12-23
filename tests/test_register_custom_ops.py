b11712fa54b9708169082fce452d58fa3aa26abc
bxdd <bxd98@126.com>
2020-12-23 16:39:17 +0000

    fix cant find ops error on Windows

--- tests/test_register_custom_ops.py
+++ tests/test_register_custom_ops.py
@@ -60,11 +60,11 @@ class Distance(PairOperator):
         series_right = self.feature_right.load(instrument, start_index, end_index, freq)
         return np.abs(series_left - series_right)
 
+OpsList = [Diff, Distance]
+Operators.register(OpsList)
 
 class TestRegiterCustomOps(TestAutoData):
     def test_regiter_custom_ops(self):
-        OpsList = [Diff, Distance]
-        Operators.register(OpsList)
         instruments = ["SH600000"]
         fields = ["Diff($close)", "Distance($close, Ref($close, 1))"]
         print(D.features(instruments, fields, start_time="2010-01-01", end_time="2017-12-31", freq="day"))
