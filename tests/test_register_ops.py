a762248d986241f90da1173d525de4f7466690dc
bxdd <bxddream@gmail.com>
2021-01-22 01:06:32 +0900

    update test&docs

--- tests/test_register_ops.py
+++ tests/test_register_ops.py
@@ -63,13 +63,11 @@ class TestRegiterCustomOps(TestAutoData):
     @classmethod
     def setUpClass(cls) -> None:
         # use default data
-        provider_uri = "~/.qlib/qlib_data/cn_data_simple"  # target_dir
+        provider_uri = "~/.qlib/qlib_data/cn_data_simple_1"  # target_dir
         if not exists_qlib_data(provider_uri):
             print(f"Qlib data is not found in {provider_uri}")
 
-            GetData().qlib_data(
-                name="qlib_data_simple", region="cn", version="latest", interval="1d", target_dir=provider_uri
-            )
+            GetData().qlib_data(name="qlib_data_simple", region="cn", interval="1d", target_dir=provider_uri)
         qlib.init(provider_uri=provider_uri, custom_ops=[Diff, Distance], region=REG_CN)
 
     def test_regiter_custom_ops(self):
