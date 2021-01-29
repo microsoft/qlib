589d204f6545740a436db5955420210ef9f84a0a
bxdd <bxd98@126.com>
2021-01-29 07:13:57 +0000

    fix market

--- examples/highfreq/workflow.py
+++ examples/highfreq/workflow.py
@@ -31,7 +31,7 @@ class HighfreqWorkflow(object):
 
     SPEC_CONF = {"custom_ops": [DayLast, FFillNan, BFillNan, Date, Select, IsNull, Cut], "expression_cache": None}
 
-    MARKET = ["SH600004"]
+    MARKET = "all"
     BENCHMARK = "SH000300"
 
     start_time = "2020-09-15 00:00:00"
