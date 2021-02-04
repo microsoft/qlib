cda1d4be40642f6ffcf1cb6a9d1e85149cb71335
HUAN-PING SU <pingsutw@gmail.com>
2021-02-03 22:14:54 -0800

    Update workflow_by_code.py
    Fix typo in workflow_by_code.py

--- examples/workflow_by_code.py
+++ examples/workflow_by_code.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
         },
     }
 
-    # model initiaiton
+    # model initiation
     model = init_instance_by_config(task["model"])
     dataset = init_instance_by_config(task["dataset"])
 
