03ef918dd8efb22d4b0561b281420795ec028c89
D-X-Y <280835372@qq.com>
2021-03-08 08:24:23 +0000

    Fix bugs in count_parameters

--- qlib/contrib/model/pytorch_utils.py
+++ qlib/contrib/model/pytorch_utils.py
@@ -3,14 +3,13 @@
 
 import torch.nn as nn
 
-
 def count_parameters(models_or_parameters, unit="mb"):
     if isinstance(models_or_parameters, nn.Module):
         counts = sum(v.numel() for v in models_or_parameters.parameters())
     elif isinstance(models_or_parameters, nn.Parameter):
         counts = models_or_parameters.numel()
     elif isinstance(models_or_parameters, (list, tuple)):
-        return sum(count_parameters(x, unit) for x in model_or_parameters)
+        return sum(count_parameters(x, unit) for x in models_or_parameters)
     else:
         counts = sum(v.numel() for v in models_or_parameters)
     if unit.lower() == "mb":
