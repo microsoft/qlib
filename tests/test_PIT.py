63e4895603778238f501b1994d837bcc97b1f988
bxdd <bxddream@gmail.com>
2021-03-11 01:43:24 +0900

    black format

--- tests/test_PIT.py
+++ tests/test_PIT.py
@@ -1,6 +1,8 @@
 import qlib
+
 qlib.init(provider_uri="~/.qlib/qlib_data/us_data")
 from qlib.data import D
-instruments = ['a1x4w7']
-fields = ['PSum($$q_taxrate*$$q_totalcurrentassets, 4)/$close', "($close*$$q_taxrate)-($high*$$q_taxrate)"]
-print(D.features(instruments, fields, start_time='2020-06-01', end_time='2020-06-10', freq='day'))
+
+instruments = ["a1x4w7"]
+fields = ["PSum($$q_taxrate*$$q_totalcurrentassets, 4)/$close", "($close*$$q_taxrate)-($high*$$q_taxrate)"]
+print(D.features(instruments, fields, start_time="2020-06-01", end_time="2020-06-10", freq="day"))
