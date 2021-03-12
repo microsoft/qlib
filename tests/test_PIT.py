a2dae5c2888bfd2314ea6225cd26ef99e5a6bfab
bxdd <bxddream@gmail.com>
2021-03-12 15:56:39 +0900

    update tets_PIT

--- tests/test_PIT.py
+++ tests/test_PIT.py
@@ -1,8 +1,26 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import sys
+import unittest
 import qlib
-
-qlib.init(provider_uri="~/.qlib/qlib_data/us_data")
 from qlib.data import D
+from qlib.tests import TestAutoData
+from qlib.config import REG_US
 
-instruments = ["a1x4w7"]
-fields = ["PSum($$q_taxrate*$$q_totalcurrentassets, 4)/$close", "$close*$$q_taxrate-$high*$$q_taxrate"]
-print(D.features(instruments, fields, start_time="2020-06-01", end_time="2020-06-10", freq="day"))
+
+class TestRegiterCustomOps(TestAutoData):
+    @classmethod
+    def setUpClass(cls) -> None:
+        # use default data
+        provider_uri = "~/.qlib/qlib_data/us_data"  # target_dir
+        qlib.init(provider_uri=provider_uri, region=REG_US)
+
+    def test_regiter_custom_ops(self):
+        instruments = ["a1x4w7"]
+        fields = ["PSum($$q_taxrate*$$q_totalcurrentassets, 4)/$close", "$close*$$q_taxrate-$high*$$q_taxrate"]
+        print(D.features(instruments, fields, start_time="2020-06-01", end_time="2020-06-10", freq="day"))
+
+
+if __name__ == "__main__":
+    unittest.main()
