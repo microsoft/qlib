07434da8b02e6e3706c554c5081ce35ba382d08e
D-X-Y <280835372@qq.com>
2021-03-15 03:35:34 +0000

    Remove unused imports

--- qlib/contrib/model/pytorch_nn.py
+++ qlib/contrib/model/pytorch_nn.py
@@ -19,7 +19,7 @@ from ...model.base import Model
 from ...data.dataset import DatasetH
 from ...data.dataset.handler import DataHandlerLP
 from ...utils import unpack_archive_with_buffer, save_multiple_parts_file, get_or_create_path, drop_nan_by_y_index
-from ...log import get_module_logger, TimeInspector
+from ...log import get_module_logger
 from ...workflow import R
 
 
