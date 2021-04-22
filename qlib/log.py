fbff4c271a7e74f2f0b4770912abf2fb01a9354b
Jactus <dw1920@nyu.edu>
2021-04-23 00:38:45 +0800

    Remove redundant methods in meta

--- qlib/log.py
+++ qlib/log.py
@@ -13,19 +13,12 @@ from .config import C
 
 
 class MetaLogger(type):
-    def __init__(self, name, bases, dic):
-        super().__init__(name, bases, dic)
 
     def __new__(cls, name, bases, dict):
         wrapper_dict = type(logging.getLogger("MetaLogger")).__dict__.copy()
         wrapper_dict.update(dict)
         wrapper_dict["__doc__"] = logging.getLogger("MetaLogger").__doc__
         return type.__new__(cls, name, bases, wrapper_dict)
-
-    def __call__(cls, *args, **kwargs):
-        obj = cls.__new__(cls)
-        cls.__init__(cls, *args, **kwargs)
-        return obj
 
 
 class QlibLogger(metaclass=MetaLogger):
