7525854beda2c0c0303b265c97b52c994561221c
v-mingzhehan <v-mingzhehan@microsoft.com>
2021-06-22 03:47:39 +0000

    Add shortcut in init

--- qlib/backtest/__init__.py
+++ qlib/backtest/__init__.py
@@ -8,7 +8,7 @@ from .backtest import backtest_loop
 from .backtest import collect_data_loop
 
 from .order import Order
-from .utils import CommonInfrastructure
+from .utils import CommonInfrastructure, TradeCalendarManager
 from ..strategy.base import BaseStrategy
 from ..utils import init_instance_by_config
 from ..log import get_module_logger
