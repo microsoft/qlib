6e88ccca885379ab4a816bf7137cd98dd96f1742
zhupr <zhu.pengrong@foxmail.com>
2021-08-30 18:36:38 +0800

    Fix the index type of the multi-freq example

--- examples/benchmarks/LightGBM/features_sample.py
+++ examples/benchmarks/LightGBM/features_sample.py
@@ -11,6 +11,7 @@ class ResampleProcessor(InstProcessor):
         self.minute = minute
 
     def __call__(self, df: pd.DataFrame, *args, **kwargs):
+        df.index = pd.to_datetime(df.index)
         df = df.loc[df.index.time == datetime.time(self.hour, self.minute)]
         df.index = df.index.normalize()
         return df
