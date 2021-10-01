3a152f9b8b3d13a3832d1ee743cc552887af20f6
Young <afe.young@gmail.com>
2021-10-01 03:54:49 +0000

    fix CI

--- setup.py
+++ setup.py
@@ -127,7 +127,6 @@ setup(
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
