843b0e0aed67bf54664c2411841524ff14a42712
you-n-g <you-n-g@users.noreply.github.com>
2021-11-22 20:32:29 +0800

    Update workflow_by_code.py

--- examples/workflow_by_code.py
+++ examples/workflow_by_code.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
         recorder = R.get_recorder()
         sr = SignalRecord(model, dataset, recorder)
         sr.generate()
-        
+
         # Signal Analysis
         sar = SigAnaRecord(recorder)
         sar.generate()
