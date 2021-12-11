0a41343314e7b353674b69c0713bf30052ce0147
DefangCui <170007807@pku.edu.cn>
2021-12-12 00:30:38 +0800

    solve black format

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ import numpy
 
 from setuptools import find_packages, setup, Extension
 
+
 def read(rel_path: str) -> str:
     here = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(here, rel_path)) as fp:
         return fp.read()
+
 
 def get_version(rel_path: str) -> str:
     for line in read(rel_path).splitlines():
