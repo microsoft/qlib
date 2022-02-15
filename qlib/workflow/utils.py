e6b777c89df90a2b5977fd0bed1b7a02cacd06cc
Young <afe.young@gmail.com>
2022-02-15 10:39:16 +0800

    enhance pytorch_nn

--- qlib/workflow/utils.py
+++ qlib/workflow/utils.py
@@ -20,6 +20,9 @@ def experiment_exit_handler():
     The `atexit` handler should be put in the last, since, as long as the program ends, it will be called.
     Thus, if any exception or user interuption occurs beforehead, we should handle them first. Once `R` is
     ended, another call of `R.end_exp` will not take effect.
+
+    Limitations:
+    - If pdb is used in the your program, excepthook will not be triggered when it ends.  The status will be finished
     """
     sys.excepthook = experiment_exception_hook  # handle uncaught exception
     atexit.register(R.end_exp, recorder_status=Recorder.STATUS_FI)  # will not take effect if experiment ends
