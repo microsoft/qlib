97a6b3fcecc0f773df5a4d2c4829568a5a4b4ac3
Wangwuyi123 <51237097+Wangwuyi123@users.noreply.github.com>
2022-03-10 19:54:24 +0800

    Update collector.py

--- scripts/data_collector/yahoo/collector.py
+++ scripts/data_collector/yahoo/collector.py
@@ -126,7 +126,11 @@ class YahooCollector(BaseCollector):
 
         interval = "1m" if interval in ["1m", "1min"] else interval
         try:
-            _resp = Ticker(symbol, asynchronous=False).history(interval=interval, start=start, end=end)
+            rs = pd.DataFrame(data_list, columns=rs.fields)
+            data_list = []
+            while (rs.error_code == '0') & rs.next():
+                data_list.append(rs.get_row_data())
+            _resp = pd.DataFrame(data_list, columns=rs.fields)
             if isinstance(_resp, pd.DataFrame):
                 return _resp.reset_index()
             elif isinstance(_resp, dict):
@@ -139,7 +143,6 @@ class YahooCollector(BaseCollector):
                 _show_logging_func()
         except Exception as e:
             logger.warning(f"{error_msg}:{e}" + "you can Switch to a foreign network or use another data source")
-
     def get_data(
         self, symbol: str, interval: str, start_datetime: pd.Timestamp, end_datetime: pd.Timestamp
     ) -> pd.DataFrame:
