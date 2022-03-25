0a84b453e30aa449a9517b62c3f89589f21bab45
Wangwuyi123 <51237097+Wangwuyi123@users.noreply.github.com>
2022-03-25 14:30:42 +0800

    Update test_yahoo.py

--- scripts/data_collector/yahoo/test_yahoo.py
+++ scripts/data_collector/yahoo/test_yahoo.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 import unittest
-from collector import YahooCollector,YahooCollectorCN
+from collector import YahooCollector, YahooCollectorCN
 import sys
 from pathlib import Path
 
@@ -17,8 +17,8 @@ class Test_Yahoo(unittest.TestCase):
     def check_same(self, a, b):
         self.assertEqual(self.to_str(a), self.to_str(b))
 
-    def to_symbol(self, str):
-        return ".".join('%s' % id for id in (str[2:], str[:2]))
+    def to_symbol(self, obj):
+        return ".".join("%s" % id for id in (obj[2:], obj[:2]))
 
     def test_nomal(self):
         self.instruments = "sz000017"
@@ -27,10 +27,9 @@ class Test_Yahoo(unittest.TestCase):
         self.end = "2021-06-20"
         self.fields = ["low", "open", "high", "close", "volume", "adjclose"]
         symbol = self.to_symbol(self.instruments)
-        yahoo_data = YahooCollector.get_data_from_remote(symbol=symbol,
-                                                         interval=self.interval,
-                                                         start=self.start,
-                                                         end=self.end)
+        yahoo_data = YahooCollector.get_data_from_remote(
+            symbol=symbol, interval=self.interval, start=self.start, end=self.end
+        )
         order = ["symbol", "date", "high", "volume", "low", "close", "open", "adjclose"]
         yahoo_data = yahoo_data[order]
         data = """
@@ -46,10 +45,9 @@ class Test_Yahoo(unittest.TestCase):
     def test_delisted(self):
         instruments = ["sz000018"]
         symbol = self.to_symbol(instruments)
-        yahoo_data = YahooCollector.get_data_from_remote(symbol=symbol,
-                                                         interval=self.interval,
-                                                         start=self.start,
-                                                         end=self.end)
+        yahoo_data = YahooCollector.get_data_from_remote(
+            symbol=symbol, interval=self.interval, start=self.start, end=self.end
+        )
         data = None
         self.check_same(data, yahoo_data)
 
@@ -59,7 +57,17 @@ class Test_Yahoo(unittest.TestCase):
         self.check_same(normalize_symbol, symbol)
 
     def test_get_instrument_list(self):
-        symbols_list=['000001.sz', '000002.sz', '000004.sz', '000005.sz', '000006.sz', '000007.sz', '000008.sz', '000009.sz', '000010.sz']
+        symbols_list = [
+            "000001.sz",
+            "000002.sz",
+            "000004.sz",
+            "000005.sz",
+            "000006.sz",
+            "000007.sz",
+            "000008.sz",
+            "000009.sz",
+            "000010.sz",
+        ]
         instrument_list = YahooCollectorCN().get_instrument_list()
         instrument_list = instrument_list[:9]
         self.check_same(symbols_list, instrument_list)
