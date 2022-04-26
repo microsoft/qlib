f9d6d2fc0404b744c4628f389227339a25e8a95a
Yuge Zhang <scottyugochang@gmail.com>
2022-04-26 05:38:28 +0000

    .

--- qlib/typehint.py
+++ qlib/typehint.py
@@ -9,7 +9,7 @@ from typing import TYPE_CHECKING
 
 __all__ = ["Literal", "TypedDict", "final"]
 
-if TYPE_CHECKING or sys.version_info >= (3, 8):
+if sys.version_info >= (3, 8):
     from typing import Literal, TypedDict, final  # type: ignore  # pylint: disable=no-name-in-module
 else:
     from typing_extensions import Literal, TypedDict, final
