1f85487a4f067e05eb42317cb0a440cc10e28480
Yuge Zhang <scottyugochang@gmail.com>
2022-06-08 03:10:28 +0000

    .

--- qlib/rl/trainer/vessel.py
+++ qlib/rl/trainer/vessel.py
@@ -24,6 +24,10 @@ if TYPE_CHECKING:
     from .trainer import Trainer
 
 
+class SeedIteratorNotAvailable(BaseException):
+    pass
+
+
 class TrainingVessel(Generic[InitialStateType, StateType, ActType, ObsType, PolicyActType]):
     """A ship that contains simulator, interpreter, and policy, will be sent to trainer.
     This class controls algorithm-related parts of training, while trainer is responible for runtime part.
@@ -43,14 +47,16 @@ class TrainingVessel(Generic[InitialStateType, StateType, ActType, ObsType, Poli
         self.trainer = weakref.proxy(trainer)  # type: ignore
 
     def train_seed_iterator(self) -> Iterable[InitialStateType]:
-        """"""
-
+        """Override this to create a seed iterator for training."""
+        raise SeedIteratorNotAvailable('Seed iterator for training is not available.')
 
     def val_seed_iterator(self) -> Iterable[InitialStateType]:
-        return DataQueue(self.val_initial_states, repeat=1)
+        """Override this to create a seed iterator for validation."""
+        raise SeedIteratorNotAvailable('Seed iterator for validation is not available.')
 
     def test_seed_iterator(self) -> Iterable[InitialStateType]:
-        return DataQueue(self.test_initial_states, repeat=1)
+        """Override this to create a seed iterator for testing."""
+        raise SeedIteratorNotAvailable('Seed iterator for testing is not available.')
 
     def train(self, vector_env: BaseVectorEnv) -> dict[str, Any]:
         collector = Collector(
@@ -94,38 +100,48 @@ class DefaultTrainingVessel(TrainingVessel):
     - ``batch_size``: Batch size in ``self.policy.update`` after each collect.
     """
 
-    def __init__(self, *,
+    def __init__(
+        self, *,
         simulator_fn: Callable[[InitialStateType], Simulator[InitialStateType, StateType, ActType]],
         state_interpreter: StateInterpreter[StateType, ObsType],
         action_interpreter: ActionInterpreter[StateType, PolicyActType, ActType],
         policy: BasePolicy,
         reward: Reward,
+        train_initial_states: Sequence[InitialStateType] | None,
+        val_initial_states: Sequence[InitialStateType] | None,
+        test_initial_states: Sequence[InitialStateType] | None,
         buffer_size: int,
         episode_per_collect: int,
         update_per_collect: int,
         batch_size: int
     ):
-        ...
-
-
-    train_initial_states: Sequence[InitialStateType]
-    val_initial_states: Sequence[InitialStateType]
-    test_initial_states: Sequence[InitialStateType]
-
-    buffer_size: int
-    episode_per_collect: int
-    update_per_collect: int
-    batch_size: int
-
+        self.simulator_fn = simulator_fn
+        self.state_interpreter = state_interpreter
+        self.action_interpreter = action_interpreter
+        self.policy = policy
+        self.reward = reward
+        self.train_initial_states = train_initial_states
+        self.val_initial_states = val_initial_states
+        self.test_initial_states = test_initial_states
+        self.buffer_size = buffer_size
+        self.episode_per_collect = episode_per_collect
+        self.update_per_collect = update_per_collect
+        self.batch_size = batch_size
 
     def train_seed_iterator(self) -> Iterable[InitialStateType]:
-        return DataQueue(self.train_initial_states, repeat=-1, shuffle=True)
+        if self.train_initial_states is not None:
+            return DataQueue(self.train_initial_states, repeat=-1, shuffle=True)
+        return super().train_seed_iterator()
 
     def val_seed_iterator(self) -> Iterable[InitialStateType]:
-        return DataQueue(self.val_initial_states, repeat=1)
+        if self.val_initial_states is not None:
+            return DataQueue(self.val_initial_states, repeat=1)
+        return super().val_seed_iterator()
 
     def test_seed_iterator(self) -> Iterable[InitialStateType]:
-        return DataQueue(self.test_initial_states, repeat=1)
+        if self.test_initial_states is not None:
+            return DataQueue(self.test_initial_states, repeat=1)
+        return super().test_seed_iterator()
 
     def train_one_collect(
         self,
