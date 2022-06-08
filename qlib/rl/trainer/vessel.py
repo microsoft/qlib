2d1d8cb4a4df07407c36d1388f582eaeb3d82709
Yuge Zhang <scottyugochang@gmail.com>
2022-06-08 01:36:57 +0000

    ckpt

--- qlib/rl/trainer/vessel.py
+++ qlib/rl/trainer/vessel.py
@@ -3,7 +3,8 @@
 
 from __future__ import annotations
 
-from typing import Callable, Generic, Iterable, TYPE_CHECKING, Sequence
+import weakref
+from typing import Callable, Generic, Iterable, TYPE_CHECKING, Sequence, Any
 
 from tianshou.data import Collector, VectorReplayBuffer
 from tianshou.env import BaseVectorEnv
@@ -34,17 +35,88 @@ class TrainingVessel(Generic[InitialStateType, StateType, ActType, ObsType, Poli
     simulator_fn: Callable[[InitialStateType], Simulator[InitialStateType, StateType, ActType]]
     state_interpreter: StateInterpreter[StateType, ObsType]
     action_interpreter: ActionInterpreter[StateType, PolicyActType, ActType]
-    train_initial_states: Sequence[InitialStateType]
-    val_initial_states: Sequence[InitialStateType]
-    test_initial_states: Sequence[InitialStateType]
     policy: BasePolicy
     reward: Reward
     trainer: Trainer
+
+    def assign_trainer(self, trainer: Trainer) -> None:
+        self.trainer = weakref.proxy(trainer)  # type: ignore
+
+    def train_seed_iterator(self) -> Iterable[InitialStateType]:
+        """"""
+
+
+    def val_seed_iterator(self) -> Iterable[InitialStateType]:
+        return DataQueue(self.val_initial_states, repeat=1)
+
+    def test_seed_iterator(self) -> Iterable[InitialStateType]:
+        return DataQueue(self.test_initial_states, repeat=1)
+
+    def train(self, vector_env: BaseVectorEnv) -> dict[str, Any]:
+        collector = Collector(
+            self.policy,
+            vector_env,
+            VectorReplayBuffer(self.buffer_size, len(vector_env))
+        )
+        self.policy.train()
+        col_result = collector.collect(n_episode=self.episode_per_collect)
+        update_result = self.policy.update(
+            0, collector.buffer, batch_size=self.batch_size, repeat=self.update_per_collect
+        )
+        return {**col_result, **update_result}
+
+    def validate(self, vector_env: FiniteVectorEnv) -> dict[str, Any]:
+        self.policy.eval()
+
+        with vector_env.collector_guard():
+            test_collector = Collector(self.policy, vector_env)
+            return test_collector.collect(n_step=INF * len(vector_env))
+
+    def test(self, vector_env: FiniteVectorEnv) -> dict[str, Any]:
+        self.policy.eval()
+
+        with vector_env.collector_guard():
+            test_collector = Collector(self.policy, vector_env)
+            return test_collector.collect(n_step=INF * len(vector_env))
+
+
+class DefaultTrainingVessel(TrainingVessel):
+    """The default implementation of training vessel.
+    
+    ``__init__`` accepts a sequence of initial states so that iterator can be created.
+    ``train``, ``validate``, ``test`` each do one collect (and also update in train).
+
+    Extra hyper-parameters (only used in train) include:
+
+    - ``buffer_size``: Size of replay buffer.
+    - ``episode_per_collect``: Episodes per collect at training.
+    - ``update_per_collect``: Number of updates in ``self.policy.update`` after each collect.
+    - ``batch_size``: Batch size in ``self.policy.update`` after each collect.
+    """
+
+    def __init__(self, *,
+        simulator_fn: Callable[[InitialStateType], Simulator[InitialStateType, StateType, ActType]],
+        state_interpreter: StateInterpreter[StateType, ObsType],
+        action_interpreter: ActionInterpreter[StateType, PolicyActType, ActType],
+        policy: BasePolicy,
+        reward: Reward,
+        buffer_size: int,
+        episode_per_collect: int,
+        update_per_collect: int,
+        batch_size: int
+    ):
+        ...
+
+
+    train_initial_states: Sequence[InitialStateType]
+    val_initial_states: Sequence[InitialStateType]
+    test_initial_states: Sequence[InitialStateType]
 
     buffer_size: int
     episode_per_collect: int
     update_per_collect: int
     batch_size: int
+
 
     def train_seed_iterator(self) -> Iterable[InitialStateType]:
         return DataQueue(self.train_initial_states, repeat=-1, shuffle=True)
@@ -58,7 +130,7 @@ class TrainingVessel(Generic[InitialStateType, StateType, ActType, ObsType, Poli
     def train_one_collect(
         self,
         vector_env: BaseVectorEnv,
-    ) -> dict:
+    ) -> dict[str, Any]:
         collector = Collector(
             self.policy,
             vector_env,
@@ -71,14 +143,14 @@ class TrainingVessel(Generic[InitialStateType, StateType, ActType, ObsType, Poli
         )
         return {**col_result, **update_result}
 
-    def validate(self, vector_env: FiniteVectorEnv) -> dict:
+    def validate(self, vector_env: FiniteVectorEnv) -> dict[str, Any]:
         self.policy.eval()
 
         with vector_env.collector_guard():
             test_collector = Collector(self.policy, vector_env)
             return test_collector.collect(n_step=INF * len(vector_env))
 
-    def test(self, vector_env: FiniteVectorEnv) -> dict:
+    def test(self, vector_env: FiniteVectorEnv) -> dict[str, Any]:
         self.policy.eval()
 
         with vector_env.collector_guard():
