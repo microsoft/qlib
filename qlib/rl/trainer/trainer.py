4db856717ab65cfeceed2b8dcc6a63f3892ae37a
Yuge Zhang <scottyugochang@gmail.com>
2022-06-10 09:05:49 +0000

    .

--- qlib/rl/trainer/trainer.py
+++ qlib/rl/trainer/trainer.py
@@ -4,31 +4,15 @@
 from __future__ import annotations
 
 import copy
-import dataclasses
-import random
-from enum import Enum
+from contextlib import AbstractContextManager
 from pathlib import Path
-from typing import Any, Callable, Dict, Optional, Tuple, Sequence, cast
+from typing import Any, Iterable, cast
 
-import numpy as np
-import pandas as pd
 import torch
-from tianshou.data import Collector, VectorReplayBuffer
-from tianshou.env import BaseVectorEnv
-from tianshou.policy import BasePolicy
-from torch.utils.data import Dataset
-from utilsd import get_output_dir, get_checkpoint_dir, setup_experiment, use_cuda
-from utilsd.experiment import print_config
-from utilsd.earlystop import EarlyStop, EarlyStopStatus
-from utilsd.logging import print_log
-from utilsd.config import RegistryConfig
 
-from qlib.rl.simulator import InitialStateType, Simulator
-from qlib.rl.interpreter import StateInterpreter, ActionInterpreter
-from qlib.rl.reward import Reward
-from qlib.rl.utils import DataQueue, EnvWrapper, FiniteEnvType, LogCollector, LogWriter, finite_env_factory
+from qlib.rl.simulator import InitialStateType
+from qlib.rl.utils import EnvWrapper, FiniteEnvType, LogCollector, LogWriter
 from qlib.log import get_module_logger
-from qlib.typehint import Literal
 
 from .callbacks import Callback
 from .vessel import TrainingVesselBase
@@ -160,377 +144,95 @@ class Trainer:
                         res[f'{typename}{retry}'] = callback
         return res
 
-    def create_env_wrapper(self):
+    def fit(self, vessel: TrainingVesselBase, ckpt_path: Path | None = None) -> None:
+        vessel.assign_trainer(self)
+
+        if ckpt_path is not None:
+            _logger.info("Resuming states from %s", str(ckpt_path))
+            self.load_state_dict(torch.load(ckpt_path))
+        else:
+            self.initialize()
+
+        self._call_callback_hooks("on_fit_start")
+
+        while self.current_iter < self.max_iters:
+            self._call_callback_hooks("on_train_start")
+
+            iterator = vessel.train_seed_iterator()
+            with 
+                vector_env = self._create_vector_env(iterator)
+                self.vessel.train(vector_env)
+
+            self._call_callback_hooks("on_train_end")
+
+            if (self.current_iter + 1) % self.val_every_n_iters == 0:
+                self._call_callback_hooks("on_validate_start")
+                with vessel.val_seed_iterator() as iterator:
+                    vector_env = self._create_vector_env(vessel.val_seed_iterator())
+                    self.vessel.validate(vector_env)
+
+                self._call_callback_hooks("on_validate_end")
+
+            self.current_iter += 1
+
+            if self.should_stop:
+                break
+
+        self._call_callback_hooks("on_fit_end")
+
+    def test(self, vessel: TrainingVesselBase) -> None:
+        vessel.assign_trainer(self)
+
+        with vessel.test_seed_iterator() as 
+        vector_env = self._create_vector_env(vessel.test_seed_iterator())
+
+        self._call_callback_hooks("on_test_start")
+        self.vessel.test(vector_env)
+        self._call_callback_hooks("on_test_end")
+
+    def _create_vector_env(self, iterator: Iterable[InitialStateType]) -> None:
+        def env_factory():
+            # FIXME: state_interpreter and action_interpreter are stateful (having a weakref of env),
+            # and could be thread unsafe.
+            # I'm not sure whether it's a design flaw.
+            # I'll rethink about this when designing the trainer.
+
+            if self.finite_env_type == "dummy":
+                # We could only experience the "threading-unsafe" problem in dummy.
+                state = copy.deepcopy(self.vessel.state_interpreter)
+                action = copy.deepcopy(self.vessel.action_interpreter)
+                rew = copy.deepcopy(self.vessel.reward)
+            else:
+                state = self.vessel.state_interpreter
+                action = self.vessel.action_interpreter
+                rew = self.vessel.reward
+
         return EnvWrapper(
             simulator_fn,
-            state_interpreter,
-            action_interpreter,
+            state,
+            action,
             seed_iterator,
-            reward,
+            rew,
             logger=LogCollector(min_loglevel=min_loglevel),
         )
 
-    def test(
-        self,
-        simulator_fn: Callable[[InitialStateType], Simulator],
-        state_interpreter: StateInterpreter,
-        action_interpreter: ActionInterpreter,
-        policy: BasePolicy,
-        initial_states: Sequence[InitialStateType],
-    ) -> None:
-        ...
-
-    def fit(self, vessel: TrainingVesselBase):
-
-        if self.checkpoint_dir is not None:
-            _resume_path = self.checkpoint_dir / "resume.pth"
-        else:
-            _resume_path = Path("/tmp/resume.pth")
-
-        def _resume():
-            nonlocal best_state_dict, cur_epoch
-            if _resume_path.exists():
-                print_log(f"Resume from checkpoint: {_resume_path}", __name__)
-                data = torch.load(_resume_path)
-                logger.load_state_dict(data["logger"])
-                val_logger.load_state_dict(data["val_logger"])
-                earlystop.load_state_dict(data["earlystop"])
-                policy.load_state_dict(data["policy"])
-                best_state_dict = data["policy_best"]
-                if hasattr(policy, "optim"):
-                    policy.optim.load_state_dict(data["optim"])
-                cur_epoch = data["epoch"]
-
-        def _checkpoint():
-            torch.save(
-                {
-                    "logger": logger.state_dict(),
-                    "val_logger": val_logger.state_dict(),
-                    "earlystop": earlystop.state_dict(),
-                    "policy": policy.state_dict(),
-                    "policy_best": best_state_dict,
-                    "optim": policy.optim.state_dict()
-                    if hasattr(policy, "optim")
-                    else None,
-                    "epoch": cur_epoch,
-                },
-                _resume_path,
-            )
-            print_log(f"Checkpoint saved to {_resume_path}", __name__)
-
-        logger = Logger(
-            episode_per_collect,
-            log_interval=500,
-            tb_prefix="train",
-            count_global="step",
+        with DataQueue(initial_states) as seed_iterator:
+        vector_env = vectorize_env(
+            env_factory,
+            finite_env_type,
+            concurrency,
+            logger,
         )
-        val_logger = Logger(len(val_dataset), log_interval=2000, tb_prefix="val")
-        earlystop = EarlyStop(patience=earlystop_patience)
-        cur_epoch = 0
-        train_env = data_fn = best_state_dict = None
-
-        _resume()
-
-        try:
-            if (
-                self.checkpoint_dir is not None
-                and self.preserve_intermediate_checkpoints
-            ):
-                torch.save(
-                    policy.state_dict(),
-                    self.checkpoint_dir / f"epoch_{cur_epoch:04d}.pth",
-                )
-
-            while cur_epoch < max_epoch:
-                cur_epoch += 1
-                if train_env is None:
-                    train_env, data_fn = env_fn(logger, train_dataset, True)
-
-                logger.reset(f"Train Epoch [{cur_epoch}/{max_epoch}] Episode")
-                val_logger.reset(f"Val Epoch [{cur_epoch}/{max_epoch}] Episode")
-
-                collector_res = self._train_epoch(
-                    policy,
-                    train_env,
-                    buffer_size=buffer_size,
-                    episode_per_collect=episode_per_collect,
-                    batch_size=batch_size,
-                    update_per_collect=update_per_collect,
-                )
-                logger.write_summary(collector_res)
-
-                if self.checkpoint_dir is not None:
-                    torch.save(policy.state_dict(), self.checkpoint_dir / "latest.pth")
-                    if self.preserve_intermediate_checkpoints:
-                        torch.save(
-                            policy.state_dict(),
-                            self.checkpoint_dir / f"epoch_{cur_epoch:04d}.pth",
-                        )
-
-                if cur_epoch == max_epoch or cur_epoch % val_every_n_collect == 0:
-                    data_fn.cleanup()  # do this to save memory
-                    train_env = data_fn = None
-
-                    val_result, _ = self.evaluate(
-                        policy, env_fn, val_dataset, val_logger
-                    )
-                    val_logger.global_step = logger.global_step  # sync two loggers
-                    val_logger.write_summary()
-
-                    es = earlystop.step(val_result)
-                    if es == EarlyStopStatus.BEST:
-                        best_state_dict = copy.deepcopy(policy.state_dict())
-                        if self.checkpoint_dir is not None:
-                            torch.save(
-                                best_state_dict, self.checkpoint_dir / "best.pth"
-                            )
-                        pd.DataFrame.from_records(val_logger.logs).to_csv(
-                            get_output_dir() / "metrics_val.csv", index=False
-                        )
-                    elif es == EarlyStopStatus.STOP:
-                        break
-
-                _checkpoint()
-
-        finally:
-            if data_fn is not None:
-                data_fn.cleanup()
-
-        if best_state_dict is not None:
-            policy.load_state_dict(best_state_dict)
-
-        return logger, val_logger
-
-        test_logger = Logger(
-            len(test_dataset), log_interval=2000, prefix="Test Episode", tb_prefix="test"
+        return EnvWrapper(
+            self.vessel.simulator_fn,
+            self.vessel.state_interpreter,
+            self.vessel.action_interpreter,
+            iterator,
+            self.vessel.reward,
+            logger=LogCollector(min_loglevel=min_loglevel),
         )
-        test_logger.global_step = train_logger.global_step
-        _, test_result = trainer.evaluate(policy, env_fn, test_dataset, test_logger)
-        test_logger.write_summary()
-        test_result.to_csv(get_output_dir() / "metrics.csv", index=False)
-        return test_result
 
-
-    def _call_callback_hooks(
-        self,
-        hook_name: str,
-        *args: Any,
-        **kwargs: Any,
-    ) -> None:
-        log.debug(f"{self.__class__.__name__}: calling callback hook: {hook_name}")
-        # TODO: remove if block in v1.8
-        if hook_name in ("on_init_start", "on_init_end"):
-            # these `Callback` hooks are the only ones that do not take a lightning module.
-            # we also don't profile bc profiler hasn't been set yet
-            for callback in self.callbacks:
-                fn = getattr(callback, hook_name)
-                if callable(fn):
-                    fn(self, *args, **kwargs)
-            return
-
-        pl_module = self.lightning_module
-        if pl_module:
-            prev_fx_name = pl_module._current_fx_name
-            pl_module._current_fx_name = hook_name
-
+    def _call_callback_hooks(self, hook_name: str, *args: Any, **kwargs: Any) -> None:
         for callback in self.callbacks:
             fn = getattr(callback, hook_name)
-            if callable(fn):
-                with self.profiler.profile(f"[Callback]{callback.state_key}.{hook_name}"):
-                    fn(self, self.lightning_module, *args, **kwargs)
-
-        if pl_module:
-            # restore current_fx when nested context
-            pl_module._current_fx_name = prev_fx_name
-
-
-    def _train_epoch(
-        self,
-        policy: BasePolicy,
-        train_env: BaseVectorEnv,
-        *,
-        buffer_size: int,
-        episode_per_collect: int,
-        batch_size: int,
-        update_per_collect: int,
-    ) -> Dict[str, Any]:
-        # 1 epoch = 1 collect
-        collector = Collector(
-            policy, train_env, VectorReplayBuffer(buffer_size, len(train_env))
-        )
-        policy.train()
-        col_result = collector.collect(n_episode=episode_per_collect)
-        update_result = policy.update(
-            0, collector.buffer, batch_size=batch_size, repeat=update_per_collect
-        )
-        return {
-            "collect/" + k: np.mean(v)
-            for k, v in {**col_result, **update_result}.items()
-        }
-
-    def fit(
-        self,
-
-        simulator_fn: Callable[[InitialStateType], Simulator],
-        state_interpreter: StateInterpreter,
-        action_interpreter: ActionInterpreter,
-        policy: BasePolicy,
-        logger: LogWriter | list[LogWriter],
-        reward: Reward
-        initial_states: Sequence[InitialStateType],
-        policy: BasePolicy,
-        env_fn: Callable[
-            [Logger, Dataset, bool], Tuple[BaseVectorEnv, DataConsumerFactory]
-        ],
-        train_dataset: Dataset,
-        val_dataset: Dataset,
-        *,
-        max_epoch: int,
-        update_per_collect: int,
-        batch_size: int,
-        episode_per_collect: int,
-        buffer_size: int = 200000,
-        earlystop_patience: int = 5,
-        val_every_n_collect: int = 1,
-    ) -> Tuple[Logger, Logger]:
-        if self.checkpoint_dir is not None:
-            _resume_path = self.checkpoint_dir / "resume.pth"
-        else:
-            _resume_path = Path("/tmp/resume.pth")
-
-        def _resume():
-            nonlocal best_state_dict, cur_epoch
-            if _resume_path.exists():
-                print_log(f"Resume from checkpoint: {_resume_path}", __name__)
-                data = torch.load(_resume_path)
-                logger.load_state_dict(data["logger"])
-                val_logger.load_state_dict(data["val_logger"])
-                earlystop.load_state_dict(data["earlystop"])
-                policy.load_state_dict(data["policy"])
-                best_state_dict = data["policy_best"]
-                if hasattr(policy, "optim"):
-                    policy.optim.load_state_dict(data["optim"])
-                cur_epoch = data["epoch"]
-
-        def _checkpoint():
-            torch.save(
-                {
-                    "logger": logger.state_dict(),
-                    "val_logger": val_logger.state_dict(),
-                    "earlystop": earlystop.state_dict(),
-                    "policy": policy.state_dict(),
-                    "policy_best": best_state_dict,
-                    "optim": policy.optim.state_dict()
-                    if hasattr(policy, "optim")
-                    else None,
-                    "epoch": cur_epoch,
-                },
-                _resume_path,
-            )
-            print_log(f"Checkpoint saved to {_resume_path}", __name__)
-
-        logger = Logger(
-            episode_per_collect,
-            log_interval=500,
-            tb_prefix="train",
-            count_global="step",
-        )
-        val_logger = Logger(len(val_dataset), log_interval=2000, tb_prefix="val")
-        earlystop = EarlyStop(patience=earlystop_patience)
-        cur_epoch = 0
-        train_env = data_fn = best_state_dict = None
-
-        _resume()
-
-        try:
-            if (
-                self.checkpoint_dir is not None
-                and self.preserve_intermediate_checkpoints
-            ):
-                torch.save(
-                    policy.state_dict(),
-                    self.checkpoint_dir / f"epoch_{cur_epoch:04d}.pth",
-                )
-
-            while cur_epoch < max_epoch:
-                cur_epoch += 1
-                if train_env is None:
-                    train_env, data_fn = env_fn(logger, train_dataset, True)
-
-                logger.reset(f"Train Epoch [{cur_epoch}/{max_epoch}] Episode")
-                val_logger.reset(f"Val Epoch [{cur_epoch}/{max_epoch}] Episode")
-
-                collector_res = self._train_epoch(
-                    policy,
-                    train_env,
-                    buffer_size=buffer_size,
-                    episode_per_collect=episode_per_collect,
-                    batch_size=batch_size,
-                    update_per_collect=update_per_collect,
-                )
-                logger.write_summary(collector_res)
-
-                if self.checkpoint_dir is not None:
-                    torch.save(policy.state_dict(), self.checkpoint_dir / "latest.pth")
-                    if self.preserve_intermediate_checkpoints:
-                        torch.save(
-                            policy.state_dict(),
-                            self.checkpoint_dir / f"epoch_{cur_epoch:04d}.pth",
-                        )
-
-                if cur_epoch == max_epoch or cur_epoch % val_every_n_collect == 0:
-                    data_fn.cleanup()  # do this to save memory
-                    train_env = data_fn = None
-
-                    val_result, _ = self.evaluate(
-                        policy, env_fn, val_dataset, val_logger
-                    )
-                    val_logger.global_step = logger.global_step  # sync two loggers
-                    val_logger.write_summary()
-
-                    es = earlystop.step(val_result)
-                    if es == EarlyStopStatus.BEST:
-                        best_state_dict = copy.deepcopy(policy.state_dict())
-                        if self.checkpoint_dir is not None:
-                            torch.save(
-                                best_state_dict, self.checkpoint_dir / "best.pth"
-                            )
-                        pd.DataFrame.from_records(val_logger.logs).to_csv(
-                            get_output_dir() / "metrics_val.csv", index=False
-                        )
-                    elif es == EarlyStopStatus.STOP:
-                        break
-
-                _checkpoint()
-
-        finally:
-            if data_fn is not None:
-                data_fn.cleanup()
-
-        if best_state_dict is not None:
-            policy.load_state_dict(best_state_dict)
-
-        return logger, val_logger
-
-    def evaluate(
-        self,
-        policy: BasePolicy,
-        env_fn: Callable[
-            [Logger, Dataset, bool], Tuple[BaseVectorEnv, DataConsumerFactory]
-        ],
-        dataset: Dataset,
-        logger: Optional[Logger] = None,
-    ):
-        if logger is None:
-            logger = Logger(len(dataset))
-        try:
-            venv, data_fn = env_fn(logger, dataset, False)
-            test_collector = Collector(policy, venv)
-            policy.eval()
-            test_collector.collect(n_step=int(1e18) * len(venv))
-        except StopIteration:
-            pass
-        finally:
-            data_fn.cleanup()
-
-        return logger.summary()["reward"], pd.DataFrame.from_records(logger.logs)
+            fn(self, self.vessel, *args, **kwargs)
