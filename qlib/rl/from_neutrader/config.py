ecb385a38184cf6dbe9d6ba00fadfd02626fe148
Huoran Li <huo53926@126.com>
2022-07-26 11:18:53 +0800

    Add copyright message

--- qlib/rl/from_neutrader/config.py
+++ qlib/rl/from_neutrader/config.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional, Tuple, Union
--- qlib/rl/order_execution/utils.py
+++ qlib/rl/order_execution/utils.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 from __future__ import annotations
 
 from typing import Any, cast, List, Optional, Tuple
