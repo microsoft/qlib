32ac99b5f29d58ad6ea1c3840591f0d6b9ed7ae9
Hyeongmin Moon <mohomin123@gmail.com>
2022-07-27 16:08:55 +0900

    Simplify the download command(microsoft#1232)

---
+++ qlib/run/get_data.py
@@ -0,0 +1,9 @@
+#  Copyright (c) Microsoft Corporation.
+#  Licensed under the MIT License.
+
+import fire
+from qlib.tests.data import GetData
+
+
+if __name__ == "__main__":
+    fire.Fire(GetData)
