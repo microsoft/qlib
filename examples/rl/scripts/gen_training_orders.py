b5ba4fe29256783affbf2dfaba22ec516972059b
Linlang <Lv.Linlang@hotmail.com>
2022-10-10 17:00:19 +0800

    optimize_CI

--- examples/rl/scripts/gen_training_orders.py
+++ examples/rl/scripts/gen_training_orders.py
@@ -23,15 +23,17 @@ for group, n in zip(("train", "valid", "test"), (args.train_size, args.valid_siz
     path = os.path.join("data", "pickle", f"backtest{group}.pkl")
     df = pickle.load(open(path, "rb")).reset_index()
     df["date"] = df["datetime"].dt.date.astype("datetime64")
-    
+
     dates = sorted(set([str(d).split(" ")[0] for d in df["date"]]))
 
-    data_df = pd.DataFrame({
-        "date": sorted(np.random.choice(dates, size=n, replace=False)),
-        "instrument": [args.stock] * n,
-        "amount": np.random.randint(low=3, high=11, size=n) * 100.0,
-        "order_type": [0] * n,
-    }).set_index(["date", "instrument"])
+    data_df = pd.DataFrame(
+        {
+            "date": sorted(np.random.choice(dates, size=n, replace=False)),
+            "instrument": [args.stock] * n,
+            "amount": np.random.randint(low=3, high=11, size=n) * 100.0,
+            "order_type": [0] * n,
+        }
+    ).set_index(["date", "instrument"])
 
     os.makedirs(os.path.join("data", "training_order_split", group), exist_ok=True)
     pickle.dump(data_df, open(os.path.join("data", "training_order_split", group, f"{args.stock}.pkl"), "wb"))
