617d35037868d0a84e7d20c91740e90959e86e72
BookSword <qianyun210603@hotmail.com>
2022-11-18 16:10:36 +0800

    fix typo and rename function param to avoid shading python keyword

--- qlib/workflow/utils.py
+++ qlib/workflow/utils.py
@@ -18,30 +18,30 @@ def experiment_exit_handler():
     """
     Method for handling the experiment when any unusual program ending occurs.
     The `atexit` handler should be put in the last, since, as long as the program ends, it will be called.
-    Thus, if any exception or user interuption occurs beforehead, we should handle them first. Once `R` is
+    Thus, if any exception or user interruption occurs beforehand, we should handle them first. Once `R` is
     ended, another call of `R.end_exp` will not take effect.
 
     Limitations:
-    - If pdb is used in the your program, excepthook will not be triggered when it ends.  The status will be finished
+    - If pdb is used in your program, excepthook will not be triggered when it ends.  The status will be finished
     """
     sys.excepthook = experiment_exception_hook  # handle uncaught exception
     atexit.register(R.end_exp, recorder_status=Recorder.STATUS_FI)  # will not take effect if experiment ends
 
 
-def experiment_exception_hook(type, value, tb):
+def experiment_exception_hook(exc_type, value, tb):
     """
     End an experiment with status to be "FAILED". This exception tries to catch those uncaught exception
     and end the experiment automatically.
 
     Parameters
-    type: Exception type
+    exc_type: Exception type
     value: Exception's value
     tb: Exception's traceback
     """
-    logger.error(f"An exception has been raised[{type.__name__}: {value}].")
+    logger.error(f"An exception has been raised[{exc_type.__name__}: {value}].")
 
     # Same as original format
     traceback.print_tb(tb)
-    print(f"{type.__name__}: {value}")
+    print(f"{exc_type.__name__}: {value}")
 
     R.end_exp(recorder_status=Recorder.STATUS_FA)
