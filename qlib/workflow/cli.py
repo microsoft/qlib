678611b7642c451404d5ec610fe13f40d4a29cc3
Cadenza-Li <128388363+Fivele-Li@users.noreply.github.com>
2023-05-11 14:08:14 +0800

    load BASE_CONFIG_PATH on absolute path & relative path;

--- qlib/workflow/cli.py
+++ qlib/workflow/cli.py
@@ -12,7 +12,9 @@ from qlib.config import C
 from qlib.model.trainer import task_train
 from qlib.utils.data import update_config
 from qlib.log import get_module_logger
+from qlib.utils import set_log_with_config
 
+set_log_with_config(C.logging_config)
 logger = get_module_logger("qrun", logging.INFO)
 
 
@@ -52,8 +54,8 @@ def workflow(config_path, experiment_name="workflow", uri_folder="mlruns"):
     User can run the whole Quant research workflow defined by a configure file
     - the code is located here ``qlib/workflow/cli.py`
 
-    User can specify a base_config file in your workflow.yml file by adding "base_config_path".
-    Qlib will load the configuration in base_config_path first, and the user only needs to update the custom fields
+    User can specify a base_config file in your workflow.yml file by adding "BASE_CONFIG_PATH".
+    Qlib will load the configuration in BASE_CONFIG_PATH first, and the user only needs to update the custom fields
     in their own workflow.yml file.
 
     For examples:
@@ -70,19 +72,25 @@ def workflow(config_path, experiment_name="workflow", uri_folder="mlruns"):
 
     base_config_path = config.get("BASE_CONFIG_PATH", None)
     if base_config_path:
-        logger.info(f"Use BASE_CONFIG: {base_config_path}")
+        logger.info(f"Use BASE_CONFIG_PATH: {base_config_path}")
         base_config_path = Path(base_config_path)
 
         # it will find config file in absolute path and relative path
-        if not base_config_path.exists():
-            raise FileNotFoundError(f"Can't find the BASE_CONFIG file: {base_config_path}")
+        if base_config_path.exists():
+            path = base_config_path
+        else:
+            logger.info(f"Can't find BASE_CONFIG_PATH base on: {Path.cwd()}, "
+                        f"try using relative path to config path: {Path(config_path).absolute()}")
+            relative_path = Path(config_path).absolute().parent.joinpath(base_config_path)
+            if relative_path.exists():
+                path = relative_path
+            else:
+                raise FileNotFoundError(f"Can't find the BASE_CONFIG file: {base_config_path}")
 
-        with open(base_config_path) as fp:
+        with open(path) as fp:
             base_config = yaml.safe_load(fp)
-    else:
-        base_config = {}
-
-    config = update_config(base_config, config)
+        logger.info(f"Load BASE_CONFIG_PATH succeed: {path.resolve()}")
+        config = update_config(base_config, config)
 
     # config the `sys` section
     sys_config(config, config_path)
