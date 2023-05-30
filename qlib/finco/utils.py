2af35d9c897b752acd2bb244ae398006bdf980ac
Xu Yang <xuyang1@microsoft.com>
2023-05-30 20:20:16 +0800

    second commit

---
+++ qlib/finco/utils.py
@@ -0,0 +1,9 @@
+import json
+
+def parse_json(response):
+    try:
+        return json.loads(response)
+    except json.decoder.JSONDecodeError:
+        pass
+
+    raise Exception(f"Failed to parse response: {response}, please report it or help us to fix it.")
