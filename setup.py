7e6bdc94934ffa8913ee3f0e9c6ccc6917ce61c4
Linlang <Lv.Linlang@hotmail.com>
2024-12-19 23:17:54 +0800

    rewrite deploy with macos

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, Extension
+from setuptools import find_packages, setup, Extension
+import tomli
 import numpy
 import os
 
@@ -16,6 +17,21 @@ def get_version(rel_path: str) -> str:
             return line.split(delim)[1]
     raise RuntimeError("Unable to find version string.")
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("pyproject.toml", "rb") as f:
+    pyproject_data = tomli.load(f)
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+project_config = pyproject_data.get("project", {})
+name = project_config.get("name", "default-package-name")
+description = project_config.get("description", "")
+dependencies = project_config.get("dependencies", [])
+classifiers = project_config.get("classifiers", [])
+python_requires = project_config.get("requires-python", ">=3.8.0")
+optional_dependencies = pyproject_data.get("project", {}).get("optional-dependencies", {})
 
 NUMPY_INCLUDE = numpy.get_include()
 
@@ -24,6 +40,23 @@ VERSION = get_version("qlib/__init__.py")
 
 setup(
     version=VERSION,
+    name=name,
+    description=description,
+    install_requires=dependencies,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    classifiers=classifiers,
+    extras_require=optional_dependencies,
+    python_requires=python_requires,
+    license="MIT Licence",
+    url="https://github.com/microsoft/qlib",
+    packages=find_packages(exclude=("tests",)),
+    entry_points={
+        "console_scripts": [
+            "qrun=qlib.workflow.cli:run",
+        ],
+    },
     ext_modules=[
         Extension(
             "qlib.data._libs.rolling",
@@ -37,5 +70,5 @@ setup(
             language="c++",
             include_dirs=[NUMPY_INCLUDE],
         ),
-    ]
+    ],
 )
