8362c711a6559df5be2de02178b8507db4e9c810
Linlang <Lv.Linlang@hotmail.com>
2025-05-07 15:30:35 +0800

    add comments

--- qlib/utils/paral.py
+++ qlib/utils/paral.py
@@ -22,6 +22,8 @@ class ParallelExt(Parallel):
         maxtasksperchild = kwargs.pop("maxtasksperchild", None)
         super(ParallelExt, self).__init__(*args, **kwargs)
         if isinstance(self._backend, MultiprocessingBackend):
+            # 2025-05-04 joblib released version 1.5.0, in which _backend_args was removed and replaced by _backend_kwargs. 
+            # Ref: https://github.com/joblib/joblib/pull/1525/files#diff-e4dff8042ce45b443faf49605b75a58df35b8c195978d4a57f4afa695b406bdc
             if joblib.__version__ < "1.5.0":
                 self._backend_args["maxtasksperchild"] = maxtasksperchild  # pylint: disable=E1101
             else:
