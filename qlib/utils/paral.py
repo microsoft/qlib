be9cd9fe2365c3ee09a17aed106ea1929d2bb329
Linlang <30293408+SunsetWolf@users.noreply.github.com>
2025-05-07 16:35:22 +0800

    fix ci error (#1921)
    * fix ci error
    
    * fix ci error
    
    * add comments
    
    * add comments

--- qlib/utils/paral.py
+++ qlib/utils/paral.py
@@ -6,6 +6,7 @@ from functools import partial
 from threading import Thread
 from typing import Callable, Text, Union
 
+import joblib
 from joblib import Parallel, delayed
 from joblib._parallel_backends import MultiprocessingBackend
 import pandas as pd
@@ -21,7 +22,12 @@ class ParallelExt(Parallel):
         maxtasksperchild = kwargs.pop("maxtasksperchild", None)
         super(ParallelExt, self).__init__(*args, **kwargs)
         if isinstance(self._backend, MultiprocessingBackend):
-            self._backend_args["maxtasksperchild"] = maxtasksperchild
+            # 2025-05-04 joblib released version 1.5.0, in which _backend_args was removed and replaced by _backend_kwargs.
+            # Ref: https://github.com/joblib/joblib/pull/1525/files#diff-e4dff8042ce45b443faf49605b75a58df35b8c195978d4a57f4afa695b406bdc
+            if joblib.__version__ < "1.5.0":
+                self._backend_args["maxtasksperchild"] = maxtasksperchild  # pylint: disable=E1101
+            else:
+                self._backend_kwargs["maxtasksperchild"] = maxtasksperchild  # pylint: disable=E1101
 
 
 def datetime_groupby_apply(
