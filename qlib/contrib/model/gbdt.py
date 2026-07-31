ad2f34900aa9c84a5dc96bee00272768644062e6
saket3395 <sakettulsan95@gmail.com>
2026-07-31 12:03:31 +0530

    fix: correct import order in gbdt.py (pylint C0411)
    Reorders imports in qlib/contrib/model/gbdt.py to follow standard
    grouping (stdlib, third-party, first-party), resolving the
    wrong-import-order (C0411) pylint warning referenced in #1007.

--- qlib/contrib/model/gbdt.py
+++ qlib/contrib/model/gbdt.py
@@ -1,16 +1,18 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+from typing import List, Text, Tuple, Union
+
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
-from typing import List, Text, Tuple, Union
+
+from qlib.workflow import R
 from ...model.base import ModelFT
 from ...data.dataset import DatasetH
 from ...data.dataset.handler import DataHandlerLP
 from ...model.interpret.base import LightGBMFInt
 from ...data.dataset.weight import Reweighter
-from qlib.workflow import R
 
 
 class LGBModel(ModelFT, LightGBMFInt):
